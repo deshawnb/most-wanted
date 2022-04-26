8198f6fcc7ea70084522df73da7d68e6b4bafed0
nicholas-wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-26 15:13:59 -0400

    added function for findPersonFamily

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function mainMenu(person, people) {
             //! TODO #2: Declare a findPersonFamily function //////////////////////////////////////////
             // HINT: Look for a people-collection stringifier utility function to help
             let personFamily = findPersonFamily(person[0], people);
-            alert(personFamily);
+            alert(displayPeople(personFamily));
             break;
         case "descendants":
             //! TODO #3: Declare a findPersonDescendants function //////////////////////////////////////////
@@ -192,3 +192,18 @@ function chars(input) {
 
 //////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
 // Any additional functions can be written below this line 👇. Happy Coding! 😁
+function findPersonFamily(person, people){
+    let myFamily = person.parents
+    
+    let family = people
+        .filter(function(el){
+            if(el.parents === myFamily){
+                return true;
+            }
+            else{
+                return false
+            }
+            
+        })
+    return family
+}
